c1e52e0e3e76d128da24d28c913e2f4a35459b69
fakhranfh <36889900+fakhranfh@users.noreply.github.com>
2018-07-31 17:40:31 +0700

    validasi login dan daftar

--- js/style-index.js
+++ js/style-index.js
@@ -38,13 +38,87 @@ $(document).ready(function () {
         return false;
     });
 
-    //munculkan modal login bila ditekan
-    $('#link-login').click(function(){
-        $('#modal-login').modal()
-    })
+    //munculkan modal login dan daftar bila ditekan
+    function loginDanDaftar(){
+        $('#link-login').click(function(){
+            $('#modal-login').modal();
+            if(localStorage.uname !== ""){
+                $('#modal-login input')[0].value = localStorage.ingatUname;
+                $('#modal-login input')[1].value = localStorage.ingatPwd;
+            }
+        });
 
-    //munculkan modal login bila ditekan
-    $('#link-daftar').click(function(){
-        $('#modal-daftar').modal()
-    })
+        $('#link-daftar').click(function(){
+            $('#modal-daftar').modal()
+        });
+    }
+
+    // ganti menu login dan daftar dengan menu username
+    function gantiMenu() {
+        $('ul[class="navbar-nav"]').html('<li class="nav-item dropdown"> <a class="nav-link dropdown-toggle" id="navbardrop-user" data-toggle="dropdown"> '+ localStorage.uname +' </a> <div class="dropdown-menu"> <a id="btn-logout" class="dropdown-item" href="index.html">Log Out</a> </div></li>');
+
+        $('#btn-logout').click(function () {
+            // hapus isi key uname dan pwd pada localStorage
+            localStorage.email = "";
+            localStorage.uname = "";
+            localStorage.pwd = "";
+        });
+    }
+
+    // menentukan tampilan navigation bagian kanan
+    if(localStorage.uname === ""){
+        loginDanDaftar(); //jika tidak login, tampilkan menu login dan daftar
+    }
+    else{
+        gantiMenu(); // jika masih login, tampilkan menu username
+    }
+
+    $('#modal-daftar button[type="submit"]').click(function () {
+        if( $('#modal-daftar input').val() !== ""){
+            if( $("#input-password-daftar").val() !== $("#input-konf-password-daftar").val()){
+                alert("Password tidak sama!");
+            }
+            else{
+                localStorage.emailDaftar = $('#input-email-daftar').val();
+                localStorage.unameDaftar = $('#input-username-daftar').val();
+                localStorage.pwdDaftar = $('#input-password-daftar').val();
+                $('#modal-daftar').modal('toggle');
+            }
+        }
+        return false;
+    });
+
+    //jika tombol 'masuk' pada modal login ditekan
+    $('#modal-login button[type="submit"]').click(function () {
+        var uname = $('#input-username-login').val();
+        var pwd = $('#input-password-login').val();
+
+        if(uname !== "" && pwd !== ""){
+            if( uname !== localStorage.unameDaftar && pwd !== localStorage.pwdDaftar){
+                alert('Username atau password salah!');
+                return false;
+            }
+            else{
+                localStorage.email = localStorage.emailDaftar;
+                localStorage.uname = localStorage.unameDaftar;
+                localStorage.pwd = localStorage.pwdDaftar;
+
+                if( $('#check-ingat-saya').prop('checked') ){
+                    localStorage.ingatEmail = localStorage.email;
+                    localStorage.ingatUname = localStorage.uname;
+                    localStorage.ingatPwd = localStorage.pwd;
+                }
+                else{
+                    localStorage.ingatEmail = "";
+                    localStorage.ingatUname = "";
+                    localStorage.ingatPwd = "";
+                }
+            }
+        }
+    });
+
+    if( $('#modal-login input')[0].value !== "undefined" && $('#modal-login input')[1].value !== "undefined"){
+        $('#modal-login input')[0].value = localStorage.ingatUname;
+        $('#modal-login input')[1].value = localStorage.ingatPwd;
+    }
 })
